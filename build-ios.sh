a96f919aec6618885454e63a2598b2baa7bad5b6
Stephane Boisson <sboisson@dotscreen.com>
2012-09-20 20:58:12 +0200

    Add build script

---
+++ build-ios.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+SCRIPT_PATH="`dirname \"$0\"`"
+SCRIPT_PATH="`( cd \"${SCRIPT_PATH}\" && pwd )`"
+export PATH="${SCRIPT_PATH}/gas-preprocessor:${PATH}"
+
+shopt -s extglob
+XCODE_HOME=$(xcode-select --print-path)
+
+if [[ -z $XCODE_HOME ]]; then 
+    echo "Could not find the XCode Command Line Tools required by this script"
+    exit 1
+elif [[ ! -x $XCODE_HOME ]]; then
+    echo "Could not find the XCode Command Line Tools in ${XCODE_HOME}"
+    exit 1
+fi
+
+modules="libavcodec libavdevice libavformat libavutil libswscale"
+
+TARGET_PATH="$SCRIPT_PATH"
+mkdir -p "$TARGET_PATH"
+for lib in $modules; do
+    rm ${TARGET_PATH}/${lib}*.a > /dev/null 2>&1
+done
+
+function build_arch() {
+    arch=$1
+    arch_code=$2
+    arch_options=$3
+
+    PLATFORM_HOME=${XCODE_HOME}/Platforms/${arch}.platform
+    SDK_HOME=(${PLATFORM_HOME}/Developer/SDKs/${arch}*.sdk)
+    TOOLS_PATH=${PLATFORM_HOME}/Developer/usr/bin/
+    EXTRA_FLAGS="-arch ${arch_code} -isysroot ${SDK_HOME}"
+
+    echo "SDK $arch: $SDK_HOME"
+    make distclean > /dev/null 2>&1 
+    ./configure --cross-prefix=${TOOLS_PATH} $FFMPEG_GENERAL_OPTIONS $arch_options --sysroot=${SDK_HOME} "--extra-cflags=${EXTRA_FLAGS}" "--extra-ldflags=${EXTRA_FLAGS}"
+    make -j3 || exit 1
+
+    for lib in $modules; do
+        mv "${lib}/${lib}.a" "${TARGET_PATH}/${lib}-${arch_code}.a"
+    done
+}
+
+function package_lib() {
+    for lib in $modules; do
+        args=""
+        for arch in i386 armv7; do
+            args="${args} -arch ${arch} ${TARGET_PATH}/${lib}-${arch}.a"
+        done
+        lipo -create $args -output "${TARGET_PATH}/$lib.a"
+    done
+}
+
+
+FFMPEG_SIMULATOR_GCC="${XCODE_HOME}/Platforms/iPhoneOS.platform/Developer/usr/bin/gcc"
+
+FFMPEG_GENERAL_OPTIONS="--enable-cross-compile --target-os=darwin --enable-pic"
+FFMPEG_GENERAL_OPTIONS="${FFMPEG_GENERAL_OPTIONS} --disable-doc --disable-ffmpeg --disable-encoders --disable-ffplay --disable-ffprobe --disable-ffserver --disable-swresample --disable-avfilter --disable-postproc"
+FFMPEG_GENERAL_OPTIONS="${FFMPEG_GENERAL_OPTIONS} --disable-debug --disable-vda --enable-small --disable-mmx"
+
+build_arch iPhoneSimulator i386 "--arch=i386"
+build_arch iPhoneOS armv7 "--cpu=cortex-a8 --arch=arm --enable-neon"
+
+package_lib
+
