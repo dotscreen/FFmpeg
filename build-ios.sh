65fbb2d812c12108eb811729b74e29acf26be3fc
David Durand <ddurand@dotscren.com>
2013-06-03 17:15:42 +0200

    Update reflecting SDK's changes

--- build-ios.sh
+++ build-ios.sh
@@ -25,17 +25,18 @@ done
 
 function build_arch() {
     arch=$1
-    arch_code=$2
-    arch_options=$3
+    vers=$2
+    arch_code=$3
+    arch_options=$4
 
     PLATFORM_HOME=${XCODE_HOME}/Platforms/${arch}.platform
-    SDK_HOME=(${PLATFORM_HOME}/Developer/SDKs/${arch}*.sdk)
+    SDK_HOME=(${PLATFORM_HOME}/Developer/SDKs/${arch}${vers}.sdk)
     TOOLS_PATH=${PLATFORM_HOME}/Developer/usr/bin/
     EXTRA_FLAGS="-arch ${arch_code} -isysroot ${SDK_HOME}"
 
     echo "SDK $arch: $SDK_HOME"
     make distclean > /dev/null 2>&1 
-    ./configure --cross-prefix=${TOOLS_PATH} $FFMPEG_GENERAL_OPTIONS $arch_options --sysroot=${SDK_HOME} "--extra-cflags=${EXTRA_FLAGS}" "--extra-ldflags=${EXTRA_FLAGS}"
+    ./configure --cross-prefix=${TOOLS_PATH} $FFMPEG_GENERAL_OPTIONS $arch_options --sysroot=${SDK_HOME} "--extra-cflags=${EXTRA_FLAGS}" "--extra-ldflags=${EXTRA_FLAGS}" "--as='gas-preprocessor/gas-preprocessor.pl ${TOOLS_PATH}/gcc'"
     make -j3 || exit 1
 
     for lib in $modules; do
@@ -58,10 +59,11 @@ FFMPEG_SIMULATOR_GCC="${XCODE_HOME}/Platforms/iPhoneOS.platform/Developer/usr/bi
 
 FFMPEG_GENERAL_OPTIONS="--enable-cross-compile --target-os=darwin --enable-pic"
 FFMPEG_GENERAL_OPTIONS="${FFMPEG_GENERAL_OPTIONS} --disable-doc --disable-ffmpeg --disable-encoders --disable-ffplay --disable-ffprobe --disable-ffserver --disable-swresample --disable-avfilter --disable-postproc"
-FFMPEG_GENERAL_OPTIONS="${FFMPEG_GENERAL_OPTIONS} --disable-debug --disable-vda --enable-small --disable-mmx"
+FFMPEG_GENERAL_OPTIONS="${FFMPEG_GENERAL_OPTIONS} --disable-debug --disable-asm"
 
-build_arch iPhoneSimulator i386 "--arch=i386"
-build_arch iPhoneOS armv7 "--cpu=cortex-a8 --arch=arm --enable-neon"
+build_arch iPhoneSimulator 6.1 i386 "--arch=i386"
+build_arch iPhoneOS 6.1 armv7 "--cpu=cortex-a8 --arch=arm --enable-neon"
+build_arch iPhoneOS 6.1 armv7s "--cpu=cortex-a9 --arch=arm --enable-neon"
 
 package_lib
 
